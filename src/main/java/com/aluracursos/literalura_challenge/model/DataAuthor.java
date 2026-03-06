a68419d6f593b0feb040cf2feb6ea497b64a5a08
castaneda <juxnpxblojpcd315@gmail.com>
2026-03-06 16:30:16 -0500

    renombramiento

---
+++ src/main/java/com/aluracursos/literalura_challenge/model/DataAuthor.java
@@ -0,0 +1,15 @@
+package com.aluracursos.literalura_challenge.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataAuthor (
+        @JsonAlias("name")
+        String nombre,
+        @JsonAlias("birth_year")
+        Integer nacimeinto,
+        @JsonAlias("death_year")
+        Integer fallecimiento
+){
+}
--- src/main/java/com/aluracursos/literalura_challenge/model/DataBook.java
+++ src/main/java/com/aluracursos/literalura_challenge/model/DataBook.java
@@ -5,12 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import java.util.List;
 
-@JsonIgnoreProperties
-public record Book (
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataBook (
         @JsonAlias("title")
         String titulo,
         @JsonAlias("authors")
-        List<Author> autor,
+        List<DataAuthor> autor,
         @JsonAlias("languages")
         List<String> lenguajes,
         @JsonAlias("download_count")
