9e9aac686c5788d9b8b331d3f7899a57dac88fe0
castaneda <juxnpxblojpcd315@gmail.com>
2026-03-06 18:56:28 -0500

    entidades de jpa para BookEntity

--- src/main/java/com/aluracursos/literalura_challenge/model/BookEntity.java
+++ src/main/java/com/aluracursos/literalura_challenge/model/BookEntity.java
@@ -1,4 +1,80 @@
 package com.aluracursos.literalura_challenge.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "libros")
 public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String titulo;
+    @Enumerated(EnumType.STRING)
+    private Idioma idioma;
+    private Double descargas;
+
+    @ManyToOne
+    private AuthorEntity autor;
+
+    public BookEntity() {}
+
+    public BookEntity(BookData data) {
+        this.titulo = data.titulo();
+        this.idioma = Idioma.fromString(data.lenguajes().get(0));
+        this.descargas = data.descargas();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("""
+                ------- LIBRO -------
+                Título: %s
+                Autor: %s
+                Idioma: %s
+                Descargas: %.1f
+                ---------------------
+                """, titulo, (autor != null ? autor.getNombre() : "Desconocido"), idioma, descargas);
+    }
+    // Getters y Setters...
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Idioma getIdioma() {
+        return idioma;
+    }
+
+    public void setIdioma(Idioma idioma) {
+        this.idioma = idioma;
+    }
+
+    public Double getDescargas() {
+        return descargas;
+    }
+
+    public void setDescargas(Double descargas) {
+        this.descargas = descargas;
+    }
+
+    public AuthorEntity getAutor() {
+        return autor;
+    }
+
+    public void setAutor(AuthorEntity autor) {
+        this.autor = autor;
+    }
 }
