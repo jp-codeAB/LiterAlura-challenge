5ed3555bfc7e382a4b769707b3178b5c4c5f169e
castaneda <juxnpxblojpcd315@gmail.com>
2026-03-06 19:07:15 -0500

    creacion de constructor de la clase menu

--- src/main/java/com/aluracursos/literalura_challenge/principal/Menu.java
+++ src/main/java/com/aluracursos/literalura_challenge/principal/Menu.java
@@ -2,6 +2,8 @@ package com.aluracursos.literalura_challenge.principal;
 
 import com.aluracursos.literalura_challenge.model.BookData;
 import com.aluracursos.literalura_challenge.model.ResultsData;
+import com.aluracursos.literalura_challenge.repository.AuthorRepository;
+import com.aluracursos.literalura_challenge.repository.BookRepository;
 import com.aluracursos.literalura_challenge.service.APIConsumer;
 import com.aluracursos.literalura_challenge.service.DataConverter;
 
@@ -13,22 +15,27 @@ public class Menu {
     private static final String URL_BASE = "https://gutendex.com/books/";
     private DataConverter dataConverter = new DataConverter();
     private String json;
+    private BookRepository bookRepo;
+    private AuthorRepository authorRepo;
+
+    public Menu(BookRepository br, AuthorRepository ar) {
+        this.bookRepo = br;
+        this.authorRepo = ar;
+    }
 
     public void mostrarMenu(){
         var opcion = -1;
         while (opcion !=0){
             var menu = """
             --------------------------------------------------
-                          Bienvenido a LiterAlura
-                          
-            Seleccione la opción que desea realizar 📚
-            1 - Buscar libro por título
+                          Bienvenido a LiterAlura 📚
+            --------------------------------------------------
+            1 - Buscar libro por título (API y Guardar)
             2 - Listar libros registrados
             3 - Listar autores registrados
-            4 - Listar autores vivos en un determinado año
+            4 - Listar autores vivos en determinado año
             5 - Listar libros por idioma
             0 - Salir
-            
             """;
             System.out.println(menu);
             opcion = sc.nextInt();
